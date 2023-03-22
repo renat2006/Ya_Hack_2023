4fd951795c126cf0975cd55eb84857e66ecc65e2
Renat <testinggame778@gmail.com>
2023-03-22 20:37:12 +0300

    2

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def request_handler():
 
     return webhook(session), 200
 
+
 def run():
     print(' * Use ngrok to tunnel your localhost to Aimylogic.'
           '\n * ./ngrok http 5000 || ngrok.exe http 5000 (windows)'
