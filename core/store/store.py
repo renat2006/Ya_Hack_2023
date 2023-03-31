98bd8000d16d579dae089c69c0317a2ce6e9a25c
Renat <testinggame778@gmail.com>
2023-03-31 19:35:05 +0300

    v 2.3 bug fix

--- core/store/store.py
+++ core/store/store.py
@@ -1,9 +1,14 @@
 stats = {
-    "human": {'strength': 4, 'dexterity': 5, 'constitution': 5, 'intelligence': 6, 'wisdom': 6, 'charisma': 8},
-    "elf": {'strength': 2, 'dexterity': 7, 'constitution': 3, 'intelligence': 7, 'wisdom': 7, 'charisma': 6},
-    "dwarf": {'strength': 7, 'dexterity': 3, 'constitution': 5, 'intelligence': 3, 'wisdom': 3, 'charisma': 4},
-    "wizard": {'strength': 3, 'dexterity': 5, 'constitution': 4, 'intelligence': 6, 'wisdom': 7, 'charisma': 5},
-    "giant": {'strength': 9, 'dexterity': 2, 'constitution': 9, 'intelligence': 2, 'wisdom': 2, 'charisma': 3}
+    "human": {'strength': 4, 'dexterity': 5, 'constitution': 5, 'intelligence': 6, 'wisdom': 6, 'charisma': 8,
+              'health': 15},
+    "elf": {'strength': 2, 'dexterity': 7, 'constitution': 3, 'intelligence': 7, 'wisdom': 7, 'charisma': 6,
+            'health': 13},
+    "dwarf": {'strength': 7, 'dexterity': 3, 'constitution': 5, 'intelligence': 3, 'wisdom': 3, 'charisma': 4,
+              'health': 17},
+    "wizard": {'strength': 3, 'dexterity': 5, 'constitution': 4, 'intelligence': 6, 'wisdom': 7, 'charisma': 5,
+               'health': 14},
+    "giant": {'strength': 9, 'dexterity': 2, 'constitution': 9, 'intelligence': 2, 'wisdom': 2, 'charisma': 3,
+              'health': 22}
 }
 
 translations = {
--- webhook.py
+++ webhook.py
@@ -2,7 +2,7 @@ import json
 import random
 from pprint import pprint
 
-from core.store import stats, hero_description, locations
+from core.store import stats, hero_description, locations, enemies, translations
 
 
 def create_table(user_id):
@@ -62,10 +62,21 @@ def webhook(session):
         session["location_number"] = 0
         hero_list = list(stats.keys())
         players = {}
+        players_data = {}
         for player in session["Names"]:
             players[player] = hero_list.pop(random.randrange(len(hero_list)))
         session["players"] = players
-        print(session["players"])
+        for player_name, hero in list(players.items()):
+            players_data[player_name] = {
+                "health": stats[hero]["health"],
+                "hero_type": hero,
+                "stats": stats[hero],
+                "inventory": []
+
+            }
+        session["players_data"] = players_data
+        print(session["players_data"])
+
         hero_prompt = "О, это же мои старые друзья: "
         for name in session["Names"]:
             hero_prompt += f"{name} - {hero_description[players[name]]}, "
@@ -78,6 +89,44 @@ def webhook(session):
         locations_data = get_location(list(locations.keys())[session["location_number"]])
         location_data = random.choice(locations_data)
         session["location_description"] = location_data[2]
+        session["cur_location_info"] = {
+            "location_type": location_data[1],
+            "mobs": list(eval(location_data[3])),
+            "artefacts": list(eval(location_data[4]))
+        }
+        print(session["cur_location_info"])
+        turns = {}
+        enemies_dict = {}
+        for count, enemy in eval(location_data[3]):
+            for num in range(count):
+                enemies_dict[f"{enemy}-{num + 1}"] = enemies[enemy]
+
+        session["cur_loc_enemies"] = enemies_dict
+        print(session["cur_loc_enemies"])
+        entities = list(session["players"].keys()) + list(session["cur_loc_enemies"].keys())
+        print(entities)
+        was = False
+        nums = list(range(len(entities)))
+        for cnt in range(len(entities)):
+            if cnt == len(list(session["players"].keys())):
+                was = True
+            if was:
+                turns[str(nums.pop(random.randrange(len(nums))))] = {"name": entities[cnt],
+                                                                     "type": str(entities[cnt].split("-")[0])}
+            else:
+                turns[str(nums.pop(random.randrange(len(nums))))] = {"name": entities[cnt], "type": "player"}
+        turns = dict(sorted(turns.items()))
+        session["turns"] = turns
+        turns_text = ""
+        for i, entity in turns.items():
+            if entity["type"] == "player":
+                turns_text += f"{i + 1} ходит {entity['name']}, "
+            else:
+                turns_text += f"{i + 1} ходит {translations[entity['name'].split('-')[0]]}-{entity['name'].split('-')[1]}, "
+        session["turns_text"] = turns_text
+
+
+
     else:
         print('Unknown action. Session data:')
         pprint(session)
