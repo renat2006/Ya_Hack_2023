af6e8b625a054d2558050c75a9defd7fe31906b6
Renat <testinggame778@gmail.com>
2023-04-02 13:16:48 +0300

    v 2.4.1 bugfix

--- webhook.py
+++ webhook.py
@@ -3,7 +3,7 @@ import random
 from pprint import pprint
 
 from core.store import stats, hero_description, locations, enemies, translations, hero_attack_prompt, \
-    enemies_attack_prompt
+    enemies_attack_prompt, abilities
 
 
 def create_table(user_id):
@@ -63,7 +63,7 @@ def roll_dice(sides=6, num_dice=1):
     return total
 
 
-def update_turns(session):
+def make_turns(session):
     turns = {}
     entities = list(session["players"].keys()) + list(session["cur_loc_enemies"].keys())
     print(entities)
@@ -77,6 +77,18 @@ def update_turns(session):
                                                                  "type": str(entities[cnt].split("-")[0])}
         else:
             turns[str(nums.pop(random.randrange(len(nums))))] = {"name": entities[cnt], "type": "player"}
+
+    return dict(sorted(turns.items()))
+
+
+def update_turns(session):
+    turns = {}
+    entities = session["turns"]
+
+    for count, name in enumerate(list(entities.values())):
+        turns[str(count)] = name
+    print("update")
+    print(turns)
     return dict(sorted(turns.items()))
 
 
@@ -112,7 +124,10 @@ def webhook(session):
     elif action == 'history':
         session["history_part"] = locations[list(locations.keys())[session["location_number"]]]["prehistory"]
 
+
+
     elif action == 'location':
+
         locations_data = get_location(list(locations.keys())[session["location_number"]])
         location_data = random.choice(locations_data)
         session["location_description"] = location_data[2]
@@ -130,8 +145,22 @@ def webhook(session):
         enemies_dict = dict(sorted(enemies_dict.items(), key=lambda item: item[1]["health"]))
         session["cur_loc_enemies"] = enemies_dict
         print(session["cur_loc_enemies"])
-        turns = update_turns(session)
+        entities = list(session["players"].keys()) + list(session["cur_loc_enemies"].keys())
+        print(entities)
+        was = False
+        nums = list(range(len(entities)))
+        for cnt in range(len(entities)):
+            if cnt == len(list(session["players"].keys())):
+                was = True
+            if was:
+                turns[str(nums.pop(random.randrange(len(nums))))] = {"name": entities[cnt],
+                                                                     "type": str(entities[cnt].split("-")[0])}
+            else:
+                turns[str(nums.pop(random.randrange(len(nums))))] = {"name": entities[cnt], "type": "player"}
+        turns = dict(sorted(turns.items()))
         session["turns"] = turns
+        print(session["turns"])
+
         turns_text = ""
         for i, entity in turns.items():
             if entity["type"] == "player":
@@ -141,8 +170,9 @@ def webhook(session):
         session["turns_text"] = turns_text
         session["cur_turn_count"] = 0
     elif action == 'turn':
-        turns = update_turns(session)
-        session["turns"] = turns
+        session["turns"] = update_turns(session)
+        pprint(session)
+
         session["last_enemies_names"] = list(
             map(lambda x: f'{x.split("-")[0]}-{x.split("-")[1]}', list(session["cur_loc_enemies"].keys())))
         session["last_enemies_names_text"] = '. '.join(session["last_enemies_names"])
@@ -150,8 +180,18 @@ def webhook(session):
         session["cur_person_type"] = session["turns"][str(session["cur_turn_count"])]["type"]
         session["cur_loc_enemies"] = dict(
             sorted(session["cur_loc_enemies"].items(), key=lambda item: item[1]["health"]))
+        print(session["cur_person_name"], session["cur_person_type"], session["turns"])
+    elif action == 'update_cur_turn_count':
+        session["cur_turn_count"] += 1
+        print(session["cur_turn_count"], session["turns"][str(session["cur_turn_count"])]["name"])
+
     elif action == 'roll_20_dice':
         session["roll_dice_result"] = roll_dice(20, 1)
+    elif action == 'update_turn_count':
+        if session['cur_turn_count'] + 1 == len(list(session["turns"].keys())):
+            session['cur_turn_count'] = 0
+        else:
+            session['cur_turn_count'] += 1
     elif action == 'target_check':
         enemies_intents = set([i.split("-")[0] for i in list(session["cur_loc_enemies"].keys())])
         target_type = 404
@@ -160,17 +200,18 @@ def webhook(session):
                 target_type = intent
         session["target_type"] = target_type
     elif action == 'enemy_move':
-        target = random.choice(list(random.choice(session['players_data'].keys())))
+        print("enemy_move")
+        target = random.choice(list(random.choice(list(session['players_data'].keys()))))
         attack_res = random.choices([1, 2], weights=[3, 1])
         if attack_res == 1:
             damage = roll_dice(4, session["cur_loc_enemies"][session["cur_person_name"]]["attack"])
-            enemy_move_prompt = f'{target}, {session["cur_person_name"]} {enemies_attack_prompt["usual"]}. Он наносит {damage} урона.'
+            enemy_move_prompt = f'{target}, {session["cur_person_name"]} {random.choice(enemies_attack_prompt["usual"])}. Он наносит {damage} урона.'
             session['players_data'][target]["health"] -= damage
         else:
-            enemy_move_prompt = f'{target}, {session["cur_person_name"]} {enemies_attack_prompt["missed"]}. Он не наносит урона.'
+            enemy_move_prompt = f'{target}, {session["cur_person_name"]} {random.choice(enemies_attack_prompt["missed"])}. Он не наносит урона.'
+        session["move_result"] = enemy_move_prompt
 
     elif action == 'attack':
-
         target = [x for x in list(session["cur_loc_enemies"].keys()) if x.split("-")[0] == session["target_type"]][0]
         attack_response = ""
         player_hero_type = session["players"][session["cur_person_name"]]
@@ -184,10 +225,12 @@ def webhook(session):
         if session["cur_loc_enemies"][target]["health"] <= 0:
             attack_response += f'{session["cur_person_name"]}, {random.choice(hero_attack_prompt[player_hero_type]["last"])}. {translations[target.split("-")[0]]}-{target.split("-")[1]} повержен'
             session["cur_loc_enemies"].pop(target)
+            session["turns"] = {key: val for key, val in session["turns"].items() if val != target}
 
         session["attack_response"] = attack_response
     elif action == 'ability':
         ability = session["players_data"][session["cur_person_name"]]["stats"]["abilities"]
+        ability = abilities[ability]
         if ability["type"] == "heal_all":
             for p in session["players_data"]:
                 p["stats"]["health"] += random.randint(1, ability["max_effect"])
@@ -214,7 +257,7 @@ def webhook(session):
 
     else:
         print('Unknown action. Session data:')
-        pprint(session)
+
         action = session['action']
 
         session['text'] = f'Webhook recieved a request, but couldn\'t handle the action' \
